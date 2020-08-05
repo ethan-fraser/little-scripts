0a2276cceafacfd56d7c12c18a5b4f56a342a70c
Ethan Fraser <ejpfraser@gmail.com>
2020-08-05 21:42:50 +1200

    Made lzw implementation actually functional

--- lzw.py
+++ lzw.py
@@ -1,34 +1,58 @@
 
-data = input("Enter input: ").strip().lower()
+def compress(data):
+    """ Compresses data into LZW encoded form. Returns encoded data as a hyphen seperated string. """
+    d = {}
+    for i in data:
+        if i not in d.keys():
+            d[i] = ord(i)
+    code = 256
+    output = ""
+    w = ""
+    for k in data:
+        joined = w + k
+        if joined in d.keys():
+            w = joined
+        else:
+            d[joined] = code
+            code += 1
+            output += str(d[w]) + "-"
+            w = k
+    output += str(d[w])
+    return output
 
-# init dictionary 
-d = {}
-for i in data:
-    if i not in d.keys():
-        d[i] = ord(i)
-code = 256
+def decompress(data):
+    """ Decompresses data outputed by compress(), i.e. a string of hyphen separated numerals. """
+    d = {}
+    for i in data.split("-"):
+        i = int(i)
+        if i not in d and i < 256:
+            d[i] = chr(i)
+    last_code = 255
+    output = ""
+    entry = ""
+    for code in [int(x) for x in data.split("-")]:
+        prev = entry
+        if code in d.keys():
+            entry = d[code]
+        else:
+            entry = prev + prev[0]
+        output += entry
+        d[last_code] = prev + entry[0]
+        last_code += 1
+    return output
 
-output = ""
 
-w = data[0]
-for i, c in enumerate(data):
-    if i == 0:
-        continue
-    if (w + c) in d.keys():
-        w += c
-    else:
-        output += str(d[w]) + "-"
-    d[w+c] = code
-    code += 1
-    w = c
-output += str(d[w])
-print(output)
+if __name__ == "__main__":
+    data = input("Enter input: ").strip().lower()
 
-# decode and print
-d2 = {y:x for x,y in d.items()}
-output2 = ""
-for key in output.split("-"):
-    output2 += d2[int(key)] + "-"
-print(output2)
+    print("\ncompressing...")
+    cmp = compress(data)
+    print(cmp)
+    len_data = len(data)
+    len_cmp = len("".join(["x" for i in range(len(cmp.split("-")))]))
+    percentage = (float(len_cmp)/float(len_data))*100
+    print(f"\nOriginal length: {len_data}\nCompressed length: {len_cmp} ({percentage:.2f}%)")
 
-print(d)
+    print("\ndecompressing...")
+    dcp = decompress(cmp)
+    print(dcp)
